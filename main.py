1f55dc8c7ee0afeeef22d8dabed09a9679e36728
rivka-yuliia <rivka.developer@gmail.com>
2025-10-07 22:19:01 +0300

    main entry point

---
+++ main.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import Config, load_config
+
+from handlers.other import router as other_router
+from handlers.user import router as user_router
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    """Entry point to config and run the bot."""
+
+    config: Config = load_config()
+
+    logging.basicConfig(
+        level=logging.getLevelName(level=config.log.level),
+        format=config.log.format
+    )
+
+    # Initialize the bot
+    logger.info('Starting bot...')
+    bot = Bot(
+        token=config.bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+    dp = Dispatcher()
+
+    # Register routers en dispatcher
+    dp.include_router(other_router)
+    dp.include_router(user_router)
+
+    # Skip old updates and run polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
